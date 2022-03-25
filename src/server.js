90884a907d22b5e88742d8ae252b3999ad38a5c8
Almeida154 <davidalmeida154of@gmail.com>
2022-03-25 17:48:34 -0300

    Second class

--- src/server.js
+++ src/server.js
@@ -4,5 +4,6 @@ const routes = require('./routes');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.listen(3000, () => console.log('🔥 Running'));
